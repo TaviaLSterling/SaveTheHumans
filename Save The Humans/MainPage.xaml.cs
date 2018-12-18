d63aa7234bcbff299a5cec49fcf84a3916282f4c
Tavia Sterling <TaviaSterling@outlook.com>
2018-12-18 10:31:41 -0700

    start game method

--- Save The Humans/MainPage.xaml.cs
+++ Save The Humans/MainPage.xaml.cs
@@ -60,7 +60,19 @@ namespace SaveTheHumans
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
-            AddEnemy();
+            StartGame();
+        }
+        private void StartGame()
+        {
+            human.IsHitTestVisible = true;
+            humanCaptured = false;
+            progressBar.Value = 0;
+            startButton.Visibility = Visibility.Collapsed;
+            playArea.Children.Clear();
+            playArea.Children.Add(target);
+            playArea.Children.Add(human);
+            enemyTimer.Start();
+            targetTimer.Start();
         }
         private void EndTheGame()
         {
